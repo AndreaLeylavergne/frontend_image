fc05aaf2971ebe47033e5ac81b8d537bc9e1400d
AndreaLeylavergne <119771926+AndreaLeylavergne@users.noreply.github.com>
2024-06-04 18:12:00 +0200

    Hello World + imports of fastapi, pydantic and tensorflow

---
+++ backend.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+import tensorflow as tf
+
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"Greeting": "Hello, World!"}
