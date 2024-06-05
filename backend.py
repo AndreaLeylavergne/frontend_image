d266775de5d0c18317b7da0e4f9993102d48a8b7
AndreaLeylavergne <119771926+AndreaLeylavergne@users.noreply.github.com>
2024-06-05 09:06:14 +0200

    retiring libs and just leaving tensorflow + numpy

--- backend.py
+++ backend.py
@@ -3,16 +3,9 @@ from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import tensorflow as tf
 import numpy as np
-import cv2
-import base64
-import io
-from PIL import Image
-import random
-import os
-import logging
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"Greeting": "Hello, Beautiful World!"}
+    return {"Greeting": "Hello, World!"}
