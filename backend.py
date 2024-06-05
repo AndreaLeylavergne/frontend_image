010f43485500f7cc56d9913482445dd099889b4c
AndreaLeylavergne <119771926+AndreaLeylavergne@users.noreply.github.com>
2024-06-05 08:32:13 +0200

    libs

--- backend.py
+++ backend.py
@@ -2,9 +2,17 @@ from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import tensorflow as tf
+import numpy as np
+import cv2
+import base64
+import io
+from PIL import Image
+import random
+import os
+import logging
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"Greeting": "Hello, World!"}
+    return {"Greeting": "Hello, Beautiful World!"}
