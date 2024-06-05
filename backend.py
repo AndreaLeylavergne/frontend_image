dbbfc2e598ade69f7c554404f1b73966b9690873
AndreaLeylavergne <119771926+AndreaLeylavergne@users.noreply.github.com>
2024-06-05 09:28:59 +0200

    adding computer vision libs to tensorflow et numpy

--- backend.py
+++ backend.py
@@ -3,9 +3,10 @@ from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import tensorflow as tf
 import numpy as np
+import cv2
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"Greeting": "Hello, World!"}
+    return {"Greeting": "Hello, Beautiful World!"}
